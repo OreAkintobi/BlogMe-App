dde904fd54754c13b1dd4ac62495fee93ef338ee
OreAkintobi <oreakintobi@gmail.com>
2020-04-20 01:26:08 +0100

    extracted forms from Edit & Create to BlogPostForm

---
+++ src/components/BlogPostForm.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+
+const BlogPostForm = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  return (
+    <View style={styles.view}>
+      <Text style={styles.label}>Enter Title:</Text>
+      <TextInput
+        style={styles.input}
+        value={title}
+        onChangeText={(text) => setTitle(text)}
+      />
+      <Text style={styles.label}>Enter Content:</Text>
+      <TextInput
+        style={styles.input}
+        value={content}
+        onChangeText={(text) => setContent(text)}
+      />
+      <Button title="Save Blog Post" onPress={() => {}} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  view: { marginVertical: 10 },
+  input: {
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: "black",
+    marginHorizontal: 10,
+    margin: 5,
+    padding: 5,
+    marginBottom: 15,
+  },
+  label: {
+    fontSize: 20,
+    marginBottom: 5,
+    marginHorizontal: 10,
+  },
+});
+
+export default BlogPostForm;
--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,57 +1,18 @@
-import React, { useContext, useState } from "react";
-import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import React, { useContext } from "react";
+import { StyleSheet } from "react-native";
 import { Context } from "../context/BlogContext";
+import BlogPostForm from "../components/BlogPostForm";
 
 const EditScreen = ({ navigation }) => {
-  const { state, addBlogPost } = useContext(Context);
+  const { state } = useContext(Context);
+
   const blogPost = state.find(
     (blogPost) => blogPost.id === navigation.getParam("id")
   );
 
-  const [title, setTitle] = useState(blogPost.title);
-  const [content, setContent] = useState(blogPost.content);
-
-  return (
-    <View>
-      <Text style={styles.label}>Edit Title:</Text>
-      <TextInput
-        style={styles.input}
-        value={title}
-        onChangeText={(newTitle) => setTitle(newTitle)}
-      />
-      <Text style={styles.label}>Edit Content:</Text>
-      <TextInput
-        style={styles.input}
-        value={content}
-        onChangeText={(newContent) => setContent(newContent)}
-      />
-      <Button
-        title="Save Blog Post"
-        onPress={() => {
-          addBlogPost(title, content, () => {
-            navigation.navigate("Index");
-          });
-        }}
-      />
-    </View>
-  );
+  return <BlogPostForm />;
 };
 
-const styles = StyleSheet.create({
-  input: {
-    fontSize: 18,
-    borderWidth: 1,
-    borderColor: "black",
-    marginHorizontal: 10,
-    margin: 5,
-    padding: 5,
-    marginBottom: 15,
-  },
-  label: {
-    fontSize: 20,
-    marginBottom: 5,
-    marginHorizontal: 10,
-  },
-});
+const styles = StyleSheet.create({});
 
 export default EditScreen;
