a0b11b7efc9d2adda9f6a3509fee2e433702669e
OreAkintobi <oreakintobi@gmail.com>
2020-04-30 20:32:12 +0100

    tapping outside textinput dismisses keyboard

--- src/components/BlogPostForm.js
+++ src/components/BlogPostForm.js
@@ -1,5 +1,13 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Button,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from "react-native";
 
 const BlogPostForm = ({ onSubmit, initialValues }) => {
   const [title, setTitle] = useState(initialValues ? initialValues.title : "");
@@ -8,21 +16,27 @@ const BlogPostForm = ({ onSubmit, initialValues }) => {
   );
 
   return (
-    <View style={styles.view}>
-      <Text style={styles.label}>Enter Title:</Text>
-      <TextInput
-        style={styles.input}
-        value={title}
-        onChangeText={(text) => setTitle(text)}
-      />
-      <Text style={styles.label}>Enter Content:</Text>
-      <TextInput
-        style={styles.input}
-        value={content}
-        onChangeText={(text) => setContent(text)}
-      />
-      <Button title="Save Blog Post" onPress={() => onSubmit(title, content)} />
-    </View>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.view}>
+        <Text style={styles.label}>Enter Title:</Text>
+        <TextInput
+          style={styles.input}
+          value={title}
+          onChangeText={(text) => setTitle(text)}
+        />
+        <Text style={styles.label}>Enter Content:</Text>
+        <TextInput
+          style={styles.blogBody}
+          multiline={true}
+          value={content}
+          onChangeText={(text) => setContent(text)}
+        />
+        <Button
+          title="Save Blog Post"
+          onPress={() => onSubmit(title, content)}
+        />
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -36,6 +50,16 @@ const styles = StyleSheet.create({
     margin: 5,
     padding: 5,
     marginBottom: 15,
+  },
+  blogBody: {
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: "black",
+    marginHorizontal: 10,
+    margin: 5,
+    padding: 5,
+    marginBottom: 15,
+    height: 200,
   },
   label: {
     fontSize: 20,
